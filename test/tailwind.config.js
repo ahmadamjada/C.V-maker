93f99a83ca0c1a9769b51afd476c4a7b134e6de0
ahmadamjada <165276048+ahmadamjada@users.noreply.github.com>
2024-12-17 22:06:22 +0500

    update the code

--- test/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./new.html', 'node_modules/preline/dist/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('preline/plugin'), require('@tailwindcss/forms')],
-}
-
