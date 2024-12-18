d494d83201774ade25deb16b7ff7001ce01e22c1
ahmadamjada <165276048+ahmadamjada@users.noreply.github.com>
2024-12-18 18:42:14 +0500

    roll back the coded

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#1e3a8a",
+        green: "#4ade80",
+        light: "#f1f5f9",
+        dark: "#1e293b",
+        red: "#f87171",
+        teal: "#14b8a6",
+        purple: "#a855f7",
+      },
+    },
+  },
+  plugins: [],
+  
+}
+
